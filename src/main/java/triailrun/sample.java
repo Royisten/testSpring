f687c41853b0a0eacb219be6a6b35d3ade03284f
royisten <nemihidomine@gmail.com>
2024-11-08 16:55:08 +0530

    Test run

---
+++ src/main/java/triailrun/sample.java
@@ -0,0 +1,10 @@
+package triailrun;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class sample {
+    public String getsample(){
+        return "God will help me";
+    }
+}
