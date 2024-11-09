a2845e2ff92216640a6ea7972787f0fa547923a4
royisten <nemihidomine@gmail.com>
2024-11-09 10:38:11 +0530

    + log msg added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,18 @@
 package com.example.demo; // Package declaration.
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication; // Import for running the application.
 import org.springframework.boot.autoconfigure.SpringBootApplication; // Import to mark this as a Spring Boot application.
-import org.springframework.context.ConfigurableApplicationContext;
-
-
 
 @SpringBootApplication // Combines @Configuration, @EnableAutoConfiguration, and @ComponentScan
                        // annotations.
 public class DemoApplication {
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args); 
-        sample welcomeMsg = (sample) context.getBean("sample");
-        System.out.println(welcomeMsg.getsample());
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        log.info("God is helping");
 
     }
 }
